34e83d12b440f8a0f8a1fd9d82eb0f0634ac7853
Rafael Mendes <rafael.mendes.sa@outlook.com>
2024-09-24 14:22:56 -0300

    correção das variáveis

--- src/app/detalhes-optin-campanha/detalhes-optin-campanha.component.ts
+++ src/app/detalhes-optin-campanha/detalhes-optin-campanha.component.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
-import { MetasCampanhasService } from '../shared/metas-campanhas.service';
-import { METAS_INFOS_ESTATICOS } from '../data/metas-campanha';
+
 import { CLIENTE } from '../data/cliente';
+import { METAS_INFOS_ESTATICOS } from '../data/metas-campanha';
+import { MetasCampanhasService } from '../shared/metas-campanhas.service';
+
 @Component({
   selector: 'app-detalhes-optin-campanha',
   templateUrl: './detalhes-optin-campanha.component.html',
   styleUrls: ['./detalhes-optin-campanha.component.scss']
 })
-export class DetalhesOptinCampanhaComponent {
-  selectedValue: number | null = null; // Armazena o valor do botão selecionado
-  objetivoConfirmado: boolean = false; // Controla o estado de confirmação do objetivo
-  inicioCard: number = 0;
-  cardsPorPagina: number = 3;
-  metasCampanhas: any[] = [];
-  cliente = CLIENTE;
+export class DetalhesOptinCampanhaComponent {  
+  // Informações mockadas
+  cliente = CLIENTE;  
   metasInfosEstaticos = METAS_INFOS_ESTATICOS;
+
+  // seleção dos cards
+  valorSelecionado: number | null = null; // Armazena o valor do botão selecionado
+  objetivoConfirmado: boolean = false; // Controla o estado de confirmação do objetivo
+  
+  // tratando as metas
+  metasCampanhas: any[] = [];
   metasConcluidas: boolean[] = new Array(this.metasCampanhas.length).fill(false); // armazenar cards com as metas concluídas    
   metaSelecionada: (number | null)[] = new Array(this.metasCampanhas.length).fill(null); // armazenar meta selecionada para cada card
+  
+  // calculo de metas (ainda em análise)
   percentualConcluido: number[] = [];
+  
+  // tentativa carrosel
+  inicioCard: number = 0;
+  cardsPorPagina: number = 3;
 
   constructor(private metasCampanhasService: MetasCampanhasService) { }
 
@@ -27,28 +38,28 @@ export class DetalhesOptinCampanhaComponent {
   }
 
   // Selecionar valor de meta
-  selecionarMeta(valorMeta: number, cardIndex: number, metaIndex: number) {
+  selecionarMeta(valorMeta: number, cardAtual: number, metaCardAtual: number) {
     // Verifica se o valor de meta já está selecionado
-    if (this.metaSelecionada[cardIndex] === metaIndex) {
+    if (this.metaSelecionada[cardAtual] === metaCardAtual) {
       // retirar seleção da meta
-      this.metaSelecionada[cardIndex] = null;
-      this.percentualConcluido[cardIndex] = 0;
-      this.desativarConfirmacao(cardIndex);
+      this.metaSelecionada[cardAtual] = null;
+      this.percentualConcluido[cardAtual] = 0;
+      this.desativarConfirmacao(cardAtual);
     } else {
       // nova meta
-      this.metaSelecionada[cardIndex] = metaIndex;
-      this.percentualConcluido[cardIndex] = this.calcularPorcentagem(valorMeta);
-      this.ativarConfirmacao(cardIndex);
+      this.metaSelecionada[cardAtual] = metaCardAtual;
+      this.percentualConcluido[cardAtual] = this.calcularPorcentagem(valorMeta);
+      this.ativarConfirmacao(cardAtual);
     }
     // Atualiza o valor da meta selecionada para o card atual
-    this.selectedValue = valorMeta;
-    console.log(`Meta ${metaIndex} selecionada: ${valorMeta}`);
-    console.log(`Porcentagem concluída para o card ${cardIndex}: ${this.percentualConcluido[cardIndex]}%`);
+    this.valorSelecionado = valorMeta;
+    console.log(`Meta ${metaCardAtual} selecionada: ${valorMeta}`);
+    console.log(`Porcentagem concluída para o card ${cardAtual}: ${this.percentualConcluido[cardAtual]}%`);
   }
 
   // ativar botão confirmar objetivo
-  ativarConfirmacao(cardIndex: number) {
-    const confirmarButton = document.getElementById(`confirmarObjetivo-${cardIndex}`);
+  ativarConfirmacao(cardAtual: number) {
+    const confirmarButton = document.getElementById(`confirmarObjetivo-${cardAtual}`);
     if (confirmarButton) {
       confirmarButton.removeAttribute('disabled');
       confirmarButton.classList.add('active');
@@ -56,8 +67,8 @@ export class DetalhesOptinCampanhaComponent {
   }
 
   // Desativar o botão confirmar objetivo
-  desativarConfirmacao(cardIndex: number) {
-    const confirmarButton = document.getElementById(`confirmarObjetivo-${cardIndex}`);
+  desativarConfirmacao(cardAtual: number) {
+    const confirmarButton = document.getElementById(`confirmarObjetivo-${cardAtual}`);
     if (confirmarButton) {
       confirmarButton.setAttribute('disabled', 'true');
       confirmarButton.classList.remove('active');
@@ -65,10 +76,10 @@ export class DetalhesOptinCampanhaComponent {
   }
 
   // confirmar o objetivo e alternar a exibição dos cards
-  confirmarObjetivo(cardIndex: number) {
+  confirmarObjetivo(cardAtual: number) {
     this.objetivoConfirmado = true;
-    const metasDiv = document.querySelector(`.infos-metas-${cardIndex}`);
-    const objetivoDiv = document.querySelector(`.infos-objetivo-${cardIndex}`);
+    const metasDiv = document.querySelector(`.infos-metas-${cardAtual}`);
+    const objetivoDiv = document.querySelector(`.infos-objetivo-${cardAtual}`);
 
     if (metasDiv && objetivoDiv) {
       metasDiv.classList.add('d-none');
@@ -82,9 +93,9 @@ export class DetalhesOptinCampanhaComponent {
   }
 
   // exibir novamente as metas
-  mudarObjetivo(cardIndex: number) {
-    const metasDiv = document.querySelector(`.infos-metas-${cardIndex}`);
-    const objetivoDiv = document.querySelector(`.infos-objetivo-${cardIndex}`);
+  mudarObjetivo(cardAtual: number) {
+    const metasDiv = document.querySelector(`.infos-metas-${cardAtual}`);
+    const objetivoDiv = document.querySelector(`.infos-objetivo-${cardAtual}`);
 
     if (metasDiv && objetivoDiv) {
       metasDiv.classList.remove('d-none');
@@ -100,7 +111,7 @@ export class DetalhesOptinCampanhaComponent {
     return `translateX(-${(this.inicioCard / this.metasCampanhas.length) * 100}%)`;
   }
 
-  proximoCard() { 
+  proximoCard() {
     if (this.inicioCard < this.metasCampanhas.length - 3) {
       this.inicioCard++;
     }
